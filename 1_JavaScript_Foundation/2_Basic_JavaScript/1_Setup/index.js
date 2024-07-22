6a1bacd5de23b7a5cabc49b13d762926718eb98e
Yash08official <yashwasankar008@gmail.com>
2024-07-22 11:56:53 +0530

    Basic Syntax file completed

--- 1_JavaScript_Foundation/2_Basic_JavaScript/1_Setup/index.js
+++ 1_JavaScript_Foundation/2_Basic_JavaScript/1_Setup/index.js
@@ -5,3 +5,5 @@ console.log("first code in js");
 // "Console" is an API which is presented to javascript by browsers and ".log" in a finction .It gets printed on my terminal
 
 // To run the file go to terminal and inside this terminal and write "node file_name" like node index.js
+
+
