1ac881efd8fe0e5cb4f4eecf24c9a9fefca4f4c6
Yash08official <yashwasankar008@gmail.com>
2024-07-26 22:19:34 +0530

    String method done completely

---
+++ 2_Core_Javascript/1_Hosting/question1.js
@@ -0,0 +1,52 @@
+// Write a JavaScript program to reverse a given string
+
+// Input: code
+
+// Output: edoc
+
+// Answer :
+
+function reverseString(str) {
+  // Split the string into an array of characters
+  let charArray = str.split("");
+
+  // Reverse the array of characters
+  let reversedArray = charArray.reverse();
+
+  // Join the array back into a string
+  let reversedString = reversedArray.join("");
+
+  // Return the reversed string
+  return reversedString;
+}
+
+// Example usage:
+let input = "code";
+let output = reverseString(input);
+console.log("Output is :", output); // Output: 'edoc'
+
+// OR
+
+function isPalindrome(str) {
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+
+  // Initialize pointers for start and end of the string
+  let start = 0;
+  let end = str.length - 1;
+
+  // Compare characters from start and end towards the middle
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+// Example usage
+console.log(isPalindrome("racecar")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
--- 2_Core_Javascript/1_Hosting/string_methods.js
+++ 2_Core_Javascript/1_Hosting/string_methods.js
@@ -3,47 +3,94 @@ let text = "Yash Wasankar CS Enginer CS CS CS ";
 console.log("Length of the string is :", text.length); // text.length
 
 text.replace("CS", "Computer");
-console.log(text); // output :  Yash Wasankar CS Enginer
+console.log("text.length is :", text); // output :  Yash Wasankar CS Enginer
 
 //for changing you have to collect it in variable
 
 const replaceString = text.replace("CS", "Computer");
-console.log(replaceString);
+console.log("text.replace is :", replaceString);
 
 const replaceAll = text.replaceAll("CS", "Computer");
-console.log(replaceAll);
+console.log("text.replaceAll is :", replaceAll);
 
 //split method
 const splitString = text.split(" ");
-console.log(splitString);
+console.log("text.split is :", splitString);
 
 let randomText = "Yash, Wasankar, CS, Engineer";
 const replaceStringSplit = randomText.split(",");
-console.log(replaceStringSplit);
+console.log("randomText.split is :", replaceStringSplit);
 
 // repeat method
-console.log(text.repeat(2));
+console.log("text.repeat is :", text.repeat(2));
 
 // trim method
 const trimText = "       Yash Wasankar   ";
 console.log(trimText.length);
 
 const trim = trimText.trim();
-console.log(trimText.trim());
+console.log("trimText.trim is :", trimText.trim());
 
 // trimStart
 const trimText1 = trimText.trimStart();
-console.log(trimText1.trimStart());
+console.log("trimText.trimStart is :", trimText1.trimStart());
 console.log(trimText1.length);
 
 // trimEnd
 const trimText2 = trimText.trimEnd();
-console.log(trimText2.trimEnd());
+console.log("trimText.trimEnd is :", trimText2.trimEnd());
 console.log(trimText2.length);
 
 // concat
 
-let firstName = "Yash";
+let firstName = "Yash ";
 let lastName = "Wasankar";
 const fullName = firstName.concat(lastName);
-console.log(fullName);
+console.log("firstName.concat(lastName) is :", fullName);
+
+// charAt : check the position of the character
+
+let oneString = "Hello Jhon Wick !!!";
+// character at the position , You have to specify the position from 0 to last
+console.log("oneString.charAt is :", oneString.charAt(0));
+//output : H
+
+// charCodeAt : Character encoding . It shows the code of the character
+//ASCII codes = A -Z starts from 65
+let oneString2 = "I am Jhon Wick !!!";
+console.log("oneString2.charCodeAt is :", oneString2.charCodeAt(6));
+console.log("oneString2.at is :", oneString2.at(-6)); // reads characters from last to first so from last 6 index or character is "c"
+
+//
+console.log("oneString2 is :", oneString2[0]);
+
+//
+oneString2[2] = "H"; // Strings are immutable . not change any character of string
+console.log("oneString2 result is :", oneString2);
+
+// If you want to extract a character from a string for that we have method called 'SLICE'
+
+// Slice : Taking a sub string from a string {Extract a portion of a string and written a new string}
+
+let textSlice = "adcdefghijklmnopqr";
+console.log("textSlice.slice(2) is :", textSlice.slice(2)); // starting c which index is 3
+console.log("textSlice.slice(4, 8) is :", textSlice.slice(4, 8)); // starting from index 4 to 7
+
+//It will also start character from back like index -1, -2,-3,...
+
+// SubString : It is same as Slice but it dont take negative indexing if you give negative indexing then it will consider it as zero
+
+console.log("textSlice.substring(4, 8) is :", textSlice.substring(4, 8));
+console.log("textSlice.substring(2) is :", textSlice.substring(2));
+
+//substr : In case of subStr we have second parameter as the length so when you say
+
+console.log(textSlice.substr(0, 4)); // "0" index and "4" characters (length of the string)
+
+// toLowercase
+const toLowercaseString = "ABCDEFGHIJKLmnopqrstuvwxyz";
+console.log("toLowercaseString is :", toLowercaseString.toLowerCase());
+
+//toUpperCase
+const toUpperCaseString = "ABCDEFGHIJKLmnopqrstuvwxyz";
+console.log("toUpperCaseString is :", toUpperCaseString.toUpperCase());
