4bc1ef30821ab083f10928a98c6af090cfa1e361
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-01 23:54:17 +0530

    Setting objects starts

---
+++ 4_Objected_Oriented_Programming/1_Classes/setting_object.js
@@ -0,0 +1,40 @@
+// settings an object
+// Settings object numbers
+
+// Let say I want to set and update the properties, how we can do it
+
+const user = {
+  names: ["Yash", "Virat"],
+  age: 21,
+  printDetails: function () {
+    console.log("Details of User");
+  },
+  greetings: function () {
+    console.log("greetings!!!!");
+  },
+  lastname: {
+    first: "Yash",
+    last: "Wasankar",
+  },
+};
+console.log("Names are :", user.names);
+console.log("age is : ", user.age);
+user.greetings();
+user.printDetails();
+// If I want to access value in object in object we can write like this ::->
+console.log("Object 2 first name :", user.lastname.first);
+console.log("Object 2 last name :", user.lastname.last);
+
+console.log(user["age"]);
+
+function logProperty(propertyName) {
+  console.log(user[propertyName]);
+}
+logProperty("age");
+
+// Let say I want to update the "names" in the object "user"
+// then you can say
+user["lastname"]["first"] = "Alice";
+// In simple way you can write it as
+ user.lastname.first = "AliceNew";
+console.log(user.lastname.first);
