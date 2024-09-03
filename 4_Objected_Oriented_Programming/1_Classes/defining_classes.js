73c46e6cb21472beca521a38cfbf051faea349d0
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-03 20:39:46 +0530

    defining classes in JavaScript

--- 4_Objected_Oriented_Programming/1_Classes/defining_classes.js
+++ 4_Objected_Oriented_Programming/1_Classes/defining_classes.js
@@ -25,17 +25,40 @@ class className {
   printDetails() {
     console.log(
       `Name and Email of the customer is : ${this.name} and  ${this.email}`
-      ); // So , will print details of customer as it is
-      // Basically you can create multiple functions whatever are required
-    }
-    // You can also have get method like
+    ); // So , will print details of customer as it is
+    // Basically you can create multiple functions whatever are required
+  }
+  // You can also have get method like
 
-    get info() {
-        // If you do this you can return whatever you want to return like name in this case
-        return this.name;
-    }// Your objects get this "info" to get the information
+  get info() {
+    // If you do this you can return whatever you want to return like name in this case
+    return this.name;
+  } // Your objects get this "info" to get the information
 }
 // So this is a basic template of a class
 
 // Now we see "HOW OBJECTS ARE CREATED FOR THE CLASS"
 
+// for the customer class there can be multiple customers s
+// So how can you create the multiple customers
+//lets say
+// You are creating a new objects for this customer class
+const customer = new className("King", "abc@gmail.com");
+// and whatever information is required to creating a object you will have to pass that
+//like in this case we have name and email
+// So this will automatically called when you used "new" keyword
+// Here you are calling a customer class using new keyword
+// and add both name and email
+// similarly you can create more customer like this
+const customer2 = new className("John", "john@gmail.com");
+// here you create 2 customers
+//if you want to print
+console.log(customer);
+console.log(customer2);
+// and you can also called all methods
+customer.printDetails();
+customer2.printDetails();
+console.log(customer.info);
+console.log(customer2.info);
+
+// This is how you define classes in javascript
