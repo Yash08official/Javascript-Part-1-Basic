79171dd0162b2756490878252180a286f5518149
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-04 00:09:25 +0530

    this intro

---
+++ 4_Objected_Oriented_Programming/2_this_keyword/this_global.js
@@ -0,0 +1,21 @@
+// This keyword we learn
+
+// When you say 
+console.log(this);
+// You are writing this line in a global scope , so this keyword in global scope always points global object
+// In browser it is window object and in node.js it is global object
+
+// What is this window object :
+// this is a global object which is present inside the browser
+
+// So whenever you run a file , global execution context gets created we know it very well
+// So when you run a file , global execution context gets created and this keyword points to global object
+// so , in the browser the global object is "Window"
+
+// And if you type "this" in the browser it will gives window
+
+// So , this keyword is always pointing to global object in global scope
+// this ==> window
+// this ==> global object
+// "this == window"  the browser gives you true
+// we will see what is the value of this inside the function
