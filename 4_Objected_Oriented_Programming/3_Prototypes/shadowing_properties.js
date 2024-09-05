78ba44413309d33b52c6d15c18b47f28b5802b52
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-05 19:04:23 +0530

    shadowing properties

--- 4_Objected_Oriented_Programming/3_Prototypes/shadowing_properties.js
+++ 4_Objected_Oriented_Programming/3_Prototypes/shadowing_properties.js
@@ -1,1 +1,8 @@
 // lets discuss about shadowing properties
+
+let myDate = new Date(2024, 11, 3);
+// let say I want to create my own method
+
+myDate.getFullYear = function () {
+  console.log("New date function added");
+};
