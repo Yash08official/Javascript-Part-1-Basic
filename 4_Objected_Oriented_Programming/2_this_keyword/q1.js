45c95cd5be2a7845c2cc35f20d19815151a09971
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-05 12:20:19 +0530

    question on methods

--- 4_Objected_Oriented_Programming/2_this_keyword/q1.js
+++ 4_Objected_Oriented_Programming/2_this_keyword/q1.js
@@ -1,3 +1,13 @@
+// Write a JavaScript function createGreeting that:
+
+// Takes two parameters: firstName (string) and lastName (string).
+// Defines an object person with properties firstName and lastName.
+// Defines a function greet within createGreeting that logs a greeting message using this.firstName and this.lastName.
+// Uses call, apply, and bind to invoke the greet function with different contexts:
+// callGreet: Uses call to invoke greet with the person object as context.
+// applyGreet: Uses apply to invoke greet with the person object and an array of arguments.
+// bindGreet: Uses bind to create a bound function bindGreet that can be invoked later with the person object.
+
 function createGreeting(firstName, lastName) {
   // Define person object with firstName and lastName properties
   const objectName = {
