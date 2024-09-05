51095c3c015244711b78c213e3e1a0446cff9afd
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-05 12:12:14 +0530

    call, apply, bind method

--- 4_Objected_Oriented_Programming/2_this_keyword/methods_of_classes.js
+++ 4_Objected_Oriented_Programming/2_this_keyword/methods_of_classes.js
@@ -52,3 +52,37 @@ let printFullName2 = function (color) {
 
 // then you can write it as
 printFullName2.call(obj3, "orange"); // you are passing parameter in it
+
+let printFullName3 = function (color, hobby) {
+  console.log(
+    `My name is ${this.firstName} ${this.lastName} and my favorite color is ${color} and my hobby is ${hobby}`
+  );
+};
+
+printFullName3.call(obj3, "red","Playing cricket");
+
+
+// This is how we used call method
+
+// Let discuss about next method which is "Apply"
+
+// Apply method is similar to call but there is a basic difference is
+
+printFullName3.apply(obj3, ["red", "Playing cricket"]);
+// You can simply put it in a array
+// And both ae used for function sharing
+
+// we have one more method "Bind"
+// It is also same but only there in case of bind you cannot directly call a function
+
+// In this , a function is return and you can call it later
+// So you can simply write it like this
+
+const printName = printFullName3.bind(obj3, "red", "Playing cricket");
+console.log(printName);
+// Here you can see that it is called later and it is not called in the same time when
+// you are calling the bind method
+printName();
+// so we write it like this
+
+
