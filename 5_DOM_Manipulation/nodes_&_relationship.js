220a4f3cf5020809c9ce79a16a25d47c8b9ce7b3
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-08 15:01:39 +0530

    Nodes and relationship in the DOM info

---
+++ 5_DOM_Manipulation/nodes_&_relationship.js
@@ -0,0 +1,10 @@
+// Nodes and relationship in the DOM
+
+// DOM tree looks, where you have a document object at the top and inside this document object, you have the other elements
+//,other nodes of the tree
+// Every tag you give in the html document it becomes node
+// we know hat our html document starts from HTML tag, so this HTML tag is a root element
+// It is the root element of the document object model
+// So inside the html,we have two child
+// which are head and body , so this are called as elements and if we are write anything inside it
+// which are called text node
