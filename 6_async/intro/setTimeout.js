7c907610ad79603ce21562d8efcc003cd454b370
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-20 22:12:48 +0530

    setTimeout.js

--- 6_async/intro/setTimeout.js
+++ 6_async/intro/setTimeout.js
@@ -12,15 +12,31 @@
 // a function which is passed as an argument
 
 setTimeout(() => {
-    console.log("Python");
+    console.log("Python 1");
 }, 1000);
 
 setTimeout(() => {
-    console.log("javascript");
+    console.log("javascript 2");
 }, 2000);
 
 setTimeout(() => {
-    console.log("Java");
+    console.log("javascript 0");
+},0);
+
+// we will see in the next video but
+// understand it is a one web Api call which is using the fetch API on browser so browser providing it
+//fetch API of browser
+fetch('https:/dummyjson.com/products/1')
+    .then(res => res.json())
+    .then(json => console.log(json));
+
+
+setTimeout(() => {
+    console.log("Java 3");
 }, 3000);
 
+console.log("Java 0");
+
 // It means that you are adding a delay and a piece of code adding in some time
+
+// we also learn microtask queue
