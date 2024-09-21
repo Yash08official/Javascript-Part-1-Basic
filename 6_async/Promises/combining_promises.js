33f8bfd3f0d938f18720f43735d67e6dc10987a1
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-22 01:12:10 +0530

    combining_promises.js

---
+++ 6_async/Promises/combining_promises.js
@@ -0,0 +1,23 @@
+// https://dummyjson.com/products/1
+// https://dummyjson.com/users/1
+// https://dummyjson.com/posts/1
+
+const firstObj = fetch("https://dummyjson.com/products/1").then((response) =>
+  response.json()
+);
+
+const secondObj = fetch("https://dummyjson.com/users/1").then((response) =>
+  response.json()
+);
+
+const thirdObj = fetch("https://dummyjson.com/posts/1").then((response) =>
+  response.json()
+);
+
+Promise.all([firstObj, secondObj, thirdObj])
+  .then((responses) => {
+    for (response of responses) {
+      console.log(responses);
+    }
+  })
+  .catch((error) => console.log(`Failed to fetch the ${error}`));
