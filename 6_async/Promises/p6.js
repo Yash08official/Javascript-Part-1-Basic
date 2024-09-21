5493e5b43760b7a65e852847cc417b89064545c2
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-22 01:34:06 +0530

    problem on promises

--- 6_async/Promises/p6.js
+++ 6_async/Promises/p6.js
@@ -1,21 +1,19 @@
-// Write a JavaScript function fetchAllData that:
+// Write a JavaScript function fetchAnyData that:
 
 // Creates three separate functions (fetchData1, fetchData2, fetchData3) that each return a Promise.
 // Each function simulates a network request using setTimeout to delay the operation by different durations:
-// fetchData1: Resolves after 1 second with a message "Data from fetchData1".
+// fetchData1: Rejects after 1 second with a message "Error from fetchData1".
 // fetchData2: Resolves after 2 seconds with a message "Data from fetchData2".
 // fetchData3: Resolves after 3 seconds with a message "Data from fetchData3".
-// Uses Promise.all() to execute all three Promises concurrently.
-// Logs the results if all Promises are resolved.
-// Catches and logs the error if any Promise is rejected.
-// Expected Output:
+// Uses Promise.any() to execute all three Promises concurrently.
+// Logs the result of the first successfully resolved Promise.
+// Catches and logs the error if all Promises are rejected.
 
-// ["Data from fetchData1", "Data from fetchData2", "Data from fetchData3"]
 function fetchData1() {
-    // Return a Promise that resolves after 1 second
-    return new Promise((resolve) => {
+    // Return a Promise that rejects after 1 second
+    return new Promise((_, reject) => {
         setTimeout(() => {
-            resolve("Data from fetchData1");
+            reject("Error from fetchData1");
         }, 1000);
     });
 }
@@ -38,16 +36,17 @@ function fetchData3() {
     });
 }
 
-function fetchAllData() {
-    // Use Promise.all() to execute all fetchData functions concurrently
-    Promise.all([fetchData1(), fetchData2(), fetchData3()])
-        .then((results) => {
-            // Handle the resolved results
-            console.log(results);
+function fetchAnyData() {
+    // Use Promise.any() to execute all fetchData functions concurrently
+    Promise.any([fetchData1(), fetchData2(), fetchData3()])
+        .then((result) => {
+            // Handle the resolved result
+            console.log(result);
         })
         .catch((error) => {
             // Handle any rejected Promise
             console.log(error);
         });
 }
-fetchAllData();
+
+fetchAnyData();
