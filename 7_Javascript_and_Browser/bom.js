ed4bcc9e79214a50af3c127fc1c6e5dfee0b6b5b
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-24 14:40:18 +0530

    Browser object model

--- 7_Javascript_and_Browser/bom.js
+++ 7_Javascript_and_Browser/bom.js
@@ -1,4 +1,14 @@
-alert("Hello Yash");
-
+alert("Hello who are you");
+alert("Enter your Name");
 const fullName = prompt("Enter your name :", "calvin"); // it is used to get a input
-console.log(fullName);
+// console.log(fullName);
+
+const isConsole = confirm(" Do you want to write your name on console");
+// console.log(isConsole);
+
+if (isConsole) {
+  console.log(fullName);
+} else {
+  console.log("Permission denied");
+}
+// this are blocking your code execution
